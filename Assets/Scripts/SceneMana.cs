10b5b5ffeb24f41a517e656ab39886d2cc5ca04e
khanh duy <khanhduy25072001@gmail.com>
2023-07-08 08:49:37 +0700

    update

--- Assets/Scripts/SceneMana.cs
+++ Assets/Scripts/SceneMana.cs
@@ -5,21 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class SceneMana : MonoBehaviour
 {
+    public static class SceneNames
+{
+    public const string Level1 = "Level1";
+    public const string Level2 = "Level2";
+    public const string VictoryScene = "VictoryScene";
+    // Add more scene names as needed
+}
+    public static void SceneManagement(){
+         string currentSceneName = SceneManager.GetActiveScene().name;
+        switch (currentSceneName)
+        {
+            case SceneNames.Level1:
+                SceneManager.LoadScene(SceneNames.Level2);
+                break;
+            
+            case SceneNames.Level2:
+                SceneManager.LoadScene(SceneNames.VictoryScene);
+                break;  
+
+        default:
+            break;
+        }
+    }
     public static void GoToMenu(){
         SceneManager.LoadScene("Menu");
     }
     public static void GoToLevel1(){
         SceneManager.LoadScene("Level1");
     }
-    public static void SceneManagement(){
-        string currentSceneName = SceneManager.GetActiveScene().name;
-        if (currentSceneName == "Level1"){
-            SceneManager.LoadScene("Level2");
-        }
-        else if (currentSceneName == "Level2"){
-            SceneManager.LoadScene("VictoryScene");
-        }
-    }
+    
     public static void GameOver(){
         SceneManager.LoadScene("GameOver");
     }
